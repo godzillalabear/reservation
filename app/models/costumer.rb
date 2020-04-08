9180164cabf37c87e3fc5c3cf40dce568444cfee
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-08 16:16:40 +0800

    has many association

--- app/models/costumer.rb
+++ app/models/costumer.rb
@@ -1,2 +1,4 @@
 class Costumer < ApplicationRecord
+  has_many :reserve_records
+  has_many :restaurants, through: :reserve_records
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  has_many :reserve_records
+  has_many :costumers, through: :reserve_records
 end
