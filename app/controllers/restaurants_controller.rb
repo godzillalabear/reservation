30865fcaae1e68d34311006e112aaaed87231330
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-11 16:14:21 +0800

    change root to welcome index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,6 @@ class RestaurantsController < ApplicationController
   def index
     @restaurant = Restaurant.all
   end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :restaurants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "application#index"
+  root to: "welcome#index"
 end
