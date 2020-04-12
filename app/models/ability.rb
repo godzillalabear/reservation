4ec2ed2feba702511bb876df4d02f30433a0b77e
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-13 00:14:04 +0800

    cancancan authorization not working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,6 +12,22 @@ class Ability
     #   else
     #     can :read, :all
     #   end
+
+
+    
+      # Define abilities for the passed in user here. For example:
+
+      user ||= User.new # guest user (not logged in)
+      if user.superadmin_role?
+        can :manage, :all
+        can :access, :rails_admin       # only allow admin users to access Rails Admin
+        can :manage, :dashboard         # allow access to dashboard
+      end
+      if user.supervisor_role?
+        can :manage, User
+      end
+
+
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
