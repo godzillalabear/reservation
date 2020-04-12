65a678dd8cee5eeb8463df519329c7eea869c97f
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-12 23:21:56 +0800

    add a superadmin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_133236) do
+ActiveRecord::Schema.define(version: 2020_04_12_134842) do
 
   create_table "costumers", force: :cascade do |t|
     t.string "name"
