105e1870127fadb13fff39aa9e7c5d5f39abb62f
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-13 17:28:18 +0800

    add class ability in ApplicationRecord

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,13 +19,13 @@ class Ability
 
       user ||= User.new # guest user (not logged in)
       if user.superadmin_role?
-        can :manage, :all
+        # can :manage, :all
         can :access, :rails_admin       # only allow admin users to access Rails Admin
-        can :manage, :dashboard         # allow access to dashboard
+        can :read, :dashboard         # allow access to dashboard
       end
-      if user.supervisor_role?
-        can :manage, User
-      end
+      # if user.supervisor_role?
+      #   can :manage, User
+      # end
 
 
     #
